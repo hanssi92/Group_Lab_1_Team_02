4ea36c1d2bbe9c1636927bf07dfe6a39e1bf06c1
sorihan0130 <han.hy@northeastern.edu>
2026-02-21 09:28:33 +0900

    TeamGreeting_group 2

---
+++ TeamGreeting/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hyungs
+ */
+public class Main {
+    
+    public static void main(String [] args){
+        HerbertDoucet.main(args);
+        Sumayyah.main(args);
+        HyeYoung.main(args);
+    }
+}
